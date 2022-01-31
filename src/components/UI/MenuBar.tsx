ab907e40e3f5c3bdaa6d342e076eafabc974287a
artursem <artur.semeniuk@gmail.com>
2022-01-31 21:30:20 +0100

    add animation

--- src/components/UI/MenuBar.tsx
+++ src/components/UI/MenuBar.tsx
@@ -8,33 +8,36 @@ import classes from './MenuBar.module.css';
 
 const MenuBar = () => {
 	const UICtx = useContext(UIContext);
-	const wrapperClasses = `${classes.menuWrapper} ${
-		UICtx.isMenuOpen ? classes.menuOpen : classes.menuClose
+	const spacerClasses = `${
+		UICtx.isMenuOpen ? classes.spacer : classes.spacerClosed
 	}`;
 	return (
 		<nav className={classes.nav}>
-			<div className={wrapperClasses}>
+			<div className={classes.menuWrapper}>
+				<div className={spacerClasses}></div>
 				<MenuButton />
-				<ul className={classes.menuList}>
-					<li className={classes.menuText}>
-						<Button onClick={UICtx.toggleGrid}>
-							{UICtx.isGridDisplay ? <IconList /> : <IconGrid />}
-						</Button>
-						display as {UICtx.isGridDisplay ? 'list' : 'grid'}
-					</li>
-					<li className={classes.menuText}>
-						<Button onClick={UICtx.toggleGrid}>
-							{UICtx.isGridDisplay ? <IconList /> : <IconGrid />}
-						</Button>
-						display as {UICtx.isGridDisplay ? 'list' : 'grid'}
-					</li>
-					<li className={classes.menuText}>
-						<Button onClick={UICtx.toggleGrid}>
-							{UICtx.isGridDisplay ? <IconList /> : <IconGrid />}
-						</Button>
-						display as {UICtx.isGridDisplay ? 'list' : 'grid'}
-					</li>
-				</ul>
+				{UICtx.isMenuOpen && (
+					<ul className={classes.menuList}>
+						<li className={classes.menuText}>
+							<Button onClick={UICtx.toggleGrid}>
+								{UICtx.isGridDisplay ? <IconList /> : <IconGrid />}
+							</Button>
+							display as {UICtx.isGridDisplay ? 'list' : 'grid'}
+						</li>
+						<li className={classes.menuText}>
+							<Button onClick={UICtx.toggleGrid}>
+								{UICtx.isGridDisplay ? <IconList /> : <IconGrid />}
+							</Button>
+							display as {UICtx.isGridDisplay ? 'list' : 'grid'}
+						</li>
+						<li className={classes.menuText}>
+							<Button onClick={UICtx.toggleGrid}>
+								{UICtx.isGridDisplay ? <IconList /> : <IconGrid />}
+							</Button>
+							display as {UICtx.isGridDisplay ? 'list' : 'grid'}
+						</li>
+					</ul>
+				)}
 			</div>
 		</nav>
 	);
