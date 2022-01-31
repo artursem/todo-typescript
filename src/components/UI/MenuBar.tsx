ca795d0ad24744c0774b0a51b7b972e654e0ac2b
artursem <artur.semeniuk@gmail.com>
2022-01-31 20:56:02 +0100

    add Menubar

--- src/components/UI/MenuBar.tsx
+++ src/components/UI/MenuBar.tsx
@@ -8,12 +8,34 @@ import classes from './MenuBar.module.css';
 
 const MenuBar = () => {
 	const UICtx = useContext(UIContext);
+	const wrapperClasses = `${classes.menuWrapper} ${
+		UICtx.isMenuOpen ? classes.menuOpen : classes.menuClose
+	}`;
 	return (
 		<nav className={classes.nav}>
-			<MenuButton>MENU</MenuButton>
-			<Button onClick={UICtx.toggleGrid}>
-				{UICtx.isGridDisplay ? <IconList /> : <IconGrid />}
-			</Button>
+			<div className={wrapperClasses}>
+				<MenuButton />
+				<ul className={classes.menuList}>
+					<li className={classes.menuText}>
+						<Button onClick={UICtx.toggleGrid}>
+							{UICtx.isGridDisplay ? <IconList /> : <IconGrid />}
+						</Button>
+						display as {UICtx.isGridDisplay ? 'list' : 'grid'}
+					</li>
+					<li className={classes.menuText}>
+						<Button onClick={UICtx.toggleGrid}>
+							{UICtx.isGridDisplay ? <IconList /> : <IconGrid />}
+						</Button>
+						display as {UICtx.isGridDisplay ? 'list' : 'grid'}
+					</li>
+					<li className={classes.menuText}>
+						<Button onClick={UICtx.toggleGrid}>
+							{UICtx.isGridDisplay ? <IconList /> : <IconGrid />}
+						</Button>
+						display as {UICtx.isGridDisplay ? 'list' : 'grid'}
+					</li>
+				</ul>
+			</div>
 		</nav>
 	);
 };
--- src/components/UI/MenuButton.tsx
+++ src/components/UI/MenuButton.tsx
@@ -1,23 +1,27 @@
-import React, { ReactNode, FC } from 'react';
+import React, { ReactNode, FC, useContext } from 'react';
+import { UIContext } from '../../store/UI-context';
 import Button from './Button';
 
-type MenuButtonProps = {
-	children: ReactNode;
-	isOpen?: boolean;
-};
+const MenuButton = () => {
+	const UICtx = useContext(UIContext);
 
-const MenuButton = ({ children, isOpen }: MenuButtonProps) => {
 	const buttonOffStyles = {
-		width: 100,
 		clipPath: 'polygon(15% 0%, 100% 0%, 100% 100%, 15% 100%, 0% 50%)',
+		width: 100,
+		transition: 'clip-path .3s ease-in',
 	};
 	const buttonOnStyles = {
-		width: 100,
-		clipPath: 'none',
+		clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%, 0% 50%)',
+		width: '2rem',
+		transition: 'clip-path .3s ease-in',
 	};
 	return (
-		<Button style={buttonOffStyles} width='100px'>
-			{children}
+		<Button
+			style={UICtx.isMenuOpen ? buttonOnStyles : buttonOffStyles}
+			width='100px'
+			onClick={UICtx.toggleMenu}
+		>
+			{UICtx.isMenuOpen ? 'M' : 'MENU'}
 		</Button>
 	);
 };
